03696333a7a423132559c726cc363f75f1c85a36
EJ <hanej93@naver.com>
2023-07-14 23:28:12 +0900

    프록시 기술과 한계 - 의존관계 주입

--- aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
+++ aop/src/test/java/hello/aop/proxyvs/ProxyCastingTest.java
@@ -34,14 +34,14 @@ public class ProxyCastingTest {
 	void cglibProxy() {
 		MemberServiceImpl target = new MemberServiceImpl();
 		ProxyFactory proxyFactory = new ProxyFactory(target);
-		proxyFactory.setProxyTargetClass(true); // CGLIB 동적 프록시
+		proxyFactory.setProxyTargetClass(true); // CGLIB 프록시
 
 		// 프록시를 인터페이스로 캐스팅 성공
 		MemberService memberServiceProxy = (MemberService) proxyFactory.getProxy();
 
 		log.info("proxy class={}", memberServiceProxy.getClass());
 
-		// CGLIB 동적 프록시를 구현 클래스로 캐스팅 시도 성공
+		// CGLIB 프록시를 구현 클래스로 캐스팅 시도 성공
 		MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServiceProxy;
 	}
 }
